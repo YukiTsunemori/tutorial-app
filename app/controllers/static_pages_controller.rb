6773738fa7581fac0baceb2b334c941f07f73c2a
Tsunemori <yuki.tsunemori1229@gmail.com>
2025-02-21 15:46:39 +0900

    modify static_pages_controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    # 仮に指定するviewがない場合、デフォルトで
+    # =>app/views/static_pages/home.html.erbが読み込まれる
   end
+
+  # app/views/コントローラ名/アクション名.html.erbがデフォルトで呼ばれる
 
   def help
   end
