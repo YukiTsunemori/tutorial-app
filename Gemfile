3e56fd209c71eb1b7f3082bd0fe36409b3d564f6
Tsunemori <yuki.tsunemori1229@gmail.com>
2025-02-23 23:08:12 +0900

    Make a basic User model (inclde secure passwords)

--- Gemfile
+++ Gemfile
@@ -2,9 +2,10 @@ source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.6"
-gem "bootstrap-sass", "3.4.1"
+gem "bootstrap-sass",  "3.4.1"
 gem "brakeman"
 gem "rails",           "7.0.4.3"
+gem "bcrypt",          "3.1.18"
 gem "sassc-rails",     "2.1.2"
 gem "sprockets-rails", "3.4.2"
 gem "importmap-rails", "1.1.5"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  # 渡された属性が存在することを検証します
+  before_save { self.email = email.downcase }
+  validates :name, presence:  true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+  format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+
+  has_secure_password
+  # このメソッドを使えば、DB内のpassword_digest属性に保存できる。
+  # passwordとpassword_confirmationが使える。
+  # authenticateメソッドが使える。
+
+  # authenticateメソッドは、引数に渡された文字列（パスワード）を
+  # ハッシュ化した値と、DB内にあるpassword_digestカラムの値を比較します。
+  # 失敗した時の戻り値はfalseだが、正しいパスだとユーザオブジェクトを返すようになる。
+  # しかしながら、authenticateがオブジェクトを返すことが重要ではなく、
+  # trueを返すことが重要。
+  validates :password, presence: true, length: { minimum: 6 }
 end
