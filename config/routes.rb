21ac5ed2dbb154145c0d174cea1ad5a3c2c04684
Tsunemori <yuki.tsunemori1229@gmail.com>
2025-02-24 00:42:19 +0900

    Finish at list 7.9

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,7 @@ Rails.application.routes.draw do
   get "/about",   to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   get "signup", to: "users#new"
-
-
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  resources :users
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
