327b33c0988947ed43add4572f5d9c1c0ed17567
Tsunemori <yuki.tsunemori1229@gmail.com>
2025-02-25 00:57:12 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,28 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    # 空のインスタンス（オブジェクト）を生成。
+    # newアクションが呼ばれた時、new.html.erbへインスタンス変数として渡される。
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # flashは特殊な変数
+      flash[:success] = "Welcome to the sample app!"
+      redirect_to @user
+      # redirect_to user_path(@user)と同等
+    else
+      render "new", status: :unprocessable_entity
+      # HTTPリクエストの422 Unprocessable Entityに対応するもの。
+    end
+  end
+
+  private
+   def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "users/new"
   # Defines the root path route ("/")
   # => (/)スラッシュはホームを表示。それがrootに対応する。
   # root "posts#index"
@@ -12,4 +11,14 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # resources :usersで、自動で生成されるルーティング機能は以下の通り。
+  # get "/users" => "users#index"          #ユーザー一覧画面を生成
+  # get "/users/:id" => "users#show"       #個別ユーザー詳細画面を生成
+  # get "/users/new" => "users#new"        #新規ユーザー登録画面を生成
+  # post "/users" => "users#create"        #新規ユーザー登録画面からの入力を受けて登録処理
+  # get "/users/:id/edit" => "users#edit"  #既存ユーザー編集画面を生成
+  # patch "/users/:id" => "users#update"   #編集画面からの入力を受けて更新処理
+  # put "/users/:id" => "users#update"     #編集画面からの入力を受けて更新処理
+  # delete "/users/:id" => "users#destroy" #一覧画面で選択されたデータを削除処理
 end
